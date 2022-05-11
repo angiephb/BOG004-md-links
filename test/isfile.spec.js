62c72711e2bf35fb31545c312974370f6574c1f5
angiephb <angie.pio30@gmail.com>
2022-05-10 20:40:02 -0500

    subiendo test de funciones y promesa readfiles

--- test/isfile.spec.js
+++ test/isfile.spec.js
@@ -1,14 +1,74 @@
 /* eslint-disable no-undef */
-const { readRoute, /* fileOrDirectory, readDirectory, readFiles, extMd, getMdFiles, getLinks */ } = require('../src/isfile.js');
+const { readRoute, fileOrDirectory, readDirectory, readFiles, extMd, getMdFiles, getLinks } = require('../src/isfile.js');
 
 describe('readRoute', () => { 
-    it('is a function', () => {
+    it('debería ser una función', () => {
         expect(typeof readRoute).toBe('function');
     });
     
-    it('deberia convertir la ruta relativa', () => {
-        let relative = './pruebas';
+    it('debería convertir la ruta relativa', () => {
+        let relative = './pruebas/';
         let absolute = 'C:\\Users\\Usuario\\Documents\\LABORATORIA\\PROYECTO MD\\BOG004-md-links\\pruebas';
         expect(readRoute(relative)).toBe(absolute);
     });
 });
+
+describe('fileOrDirectory', () => {
+    it('debería ser una función', () => {
+        expect(typeof fileOrDirectory).toBe('function');
+    });
+
+    /* it('deberia ser un archivo', () => {
+        expect(fileOrDirectory('./README.md')).toBe(true);
+    }); */
+});
+
+describe('readDirectory', () => {
+    it('debería ser una función', () => {
+        expect(typeof readDirectory).toBe('function');
+    });
+    it('deberia retornar un array con el contenido del directorio', () => {
+        expect(getMdFiles('./pruebas/').length).toBe(2);
+    }); 
+});
+
+describe('readFiles', () => {
+    it('debería ser una función', () =>{
+        expect(typeof readFiles).toBe('function');
+    });
+
+    test('debería leer un archivo', () => {
+        return readFiles('./pruebas/archivo.md').then(data => {
+            expect(data.json).not.toBe(0);
+        });
+    });
+    test('debería rechazar la promesa', () => {
+        return expect(readFiles('./package.json')).rejects.toMatch('No es archivo de extensión .md');
+    });
+});
+
+describe('extMd', () => {
+    it('debería ser una función', () => {
+        expect(typeof extMd).toBe('function');
+    });
+
+    it('debería ser true para una archivo .md', () => {
+        expect(extMd('readme.md')).toBe(true);
+    });
+});
+
+describe('getMdFiles', () => {
+    it('debería ser una función', () => {
+        expect(typeof getMdFiles).toBe('function');
+    });
+    it('deberia retornar un array de archivos .md', () => {
+        expect(getMdFiles('./readme.md').length).toBe(1);
+    }); 
+});
+
+describe('getLinks', () => {
+    it('debería ser una función', () => {
+        expect(typeof getLinks).toBe('function');
+    });
+});
+
